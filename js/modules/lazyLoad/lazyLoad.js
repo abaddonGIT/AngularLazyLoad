ba7859a7accdc002e33cc546874da1a2e923a00e
abaddonGIT <abaddongit@gmail.com>
2014-03-07 16:59:44 +0400

    Теперь можно подгружать модуль внутри контроллера

--- js/modules/lazyLoad/lazyLoad.js
+++ js/modules/lazyLoad/lazyLoad.js
@@ -5,22 +5,23 @@
  * Time: 13:10
  * Description: Подгрузка модулей по требованию, через директиву
  */
-(function () {
+(function() {
+    "use strict";
     var lazyLoad = angular.module("lazyLoad", []),
-    req = null,
-            regModules = []; //Уже зарегистрированные модули
+        req = null,
+        regModules = []; //Уже зарегистрированные модули
     if (typeof require !== 'undefined') {
         req = require;
     }
 
     //Кэш для подгружаемых шаблонов
-    lazyLoad.factory("cacheMod", function ($cacheFactory) {
+    lazyLoad.factory("cacheMod", function($cacheFactory) {
         return $cacheFactory('cacheMod', {
             capacity: "15"
         });
     });
 
-    lazyLoad.provider("$lazyLoad", ['$controllerProvider', '$compileProvider', '$filterProvider', '$provide', function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
+    lazyLoad.provider("$lazyLoad", ['$controllerProvider', '$compileProvider', '$filterProvider', '$provide', function($controllerProvider, $compileProvider, $filterProvider, $provide) {
         var modules = {}, mainApp = this.mainApp = null, loadFlag = false, myInjector = null,
             providers = {
                 $controllerProvider: $controllerProvider,
@@ -29,14 +30,13 @@
                 $provide: $provide
             };
         /*
-        * Регистрация подгруженых модулей в angular
-        * @param {type} $injector
-        * @param {type} modName
-        * @returns {undefined}
-        */
-        var registerModules = function (modules, afterRegister) {
+         * Регистрация подгруженых модулей в angular
+         * @param {type} $injector
+         * @param {type} modName
+         * @returns {undefined}
+         */
+        var registerModules = function(modules, afterRegister) {
             var modLn = modules.length;
-            console.log(modules);
             for (var j = 0; j < modLn; j++) {
                 var loc = modules[j], runBlocks = [], moduleFn, invokeQueue, ln = 0;
                 moduleFn = angular.module(loc);
@@ -57,7 +57,7 @@
                         //проводим регистрацию компонентов нашего модуля в каждом из провайдеров
                         provider[invokeArgs[1]].apply(provider, invokeArgs[2]);
                     }
-                    angular.forEach(runBlocks, function (fn) {
+                    angular.forEach(runBlocks, function(fn) {
                         myInjector.invoke(fn);
                     });
                 } catch (e) {
@@ -70,9 +70,9 @@
             afterRegister();
         };
         /*
-        * Проверка присутствия элемента в массиве
-        */
-        var inArray = function (array, val) {
+         * Проверка присутствия элемента в массиве
+         */
+        var inArray = function(array, val) {
             var ln = array.length;
             while (ln--) {
                 var loc = array[ln];
@@ -83,11 +83,11 @@
             }
         };
         /*
-        * Возвращает именя модулей, которые еще не были подгружены
-        * @param {Object} module
-        * @returns Array
-        */
-        var getRequires = function (module) {
+         * Возвращает именя модулей, которые еще не были подгружены
+         * @param {Object} module
+         * @returns Array
+         */
+        var getRequires = function(module) {
             var reqs = module.requires, ln = reqs.length, result = [];
             for (var i = 0; i < ln; i++) {
                 var loc = reqs[i];
@@ -98,7 +98,7 @@
             return result;
         };
 
-        var LoadInstance = function () {
+        var LoadInstance = function() {
             if (!(this instanceof LoadInstance)) {
                 return new LoadInstance();
             }
@@ -106,20 +106,20 @@
             this.loadModules = [];
         };
         /*
-        * Подгрузка модуля
-        * @returns {undefined}
-        */
-        LoadInstance.prototype.loadModule = function (module, afterModuleLoad) {
+         * Подгрузка модуля
+         * @returns {undefined}
+         */
+        LoadInstance.prototype.loadModule = function(module, afterModuleLoad) {
             var url = module.file, moduleName = module.name, inst = this;
             //Подгружаем сам модуль, пока без зависимостей
             if (!inArray(regModules, moduleName)) {
-                req([url], function () {
+                req([url], function() {
                     //Регистрирует подгруженный модуль
                     inst.loadModules.push(moduleName);
                     //Подгружаем зависимости если они есть
-                    inst.loadDeps(moduleName, function () {
+                    inst.loadDeps(moduleName, function() {
                         //регистрируем подгруженные модули
-                        registerModules(inst.loadModules, function () {
+                        registerModules(inst.loadModules, function() {
                             afterModuleLoad();
                         });
                     });
@@ -129,18 +129,18 @@
             }
         };
         /*
-        * Подгрузка зависимостей для модуля
-        * @param {type} module
-        * @param {type} afterDepsLoad
-        * @returns {undefined}
-        */
-        LoadInstance.prototype.loadDeps = function (moduleName, afterDepsLoad) {
+         * Подгрузка зависимостей для модуля
+         * @param {type} module
+         * @param {type} afterDepsLoad
+         * @returns {undefined}
+         */
+        LoadInstance.prototype.loadDeps = function(moduleName, afterDepsLoad) {
             var moduleFn = angular.module(moduleName),
-                        requires = getRequires(moduleFn),
-                        ln = requires.length, urls = [],
-                        inst = this;
+                requires = getRequires(moduleFn),
+                ln = requires.length, urls = [],
+                inst = this;
 
-            var _load = function (dep, i) {
+            var _load = function(dep, i) {
                 if (i === undefined) {
                     i = 0;
                 }
@@ -148,7 +148,7 @@
                 if (i < ln) {
                     var mod = requires[i];
                     if (mod) {
-                        req([mod.file], function () {
+                        req([mod.file], function() {
                             //ставим что модуль загружен
                             inst.loadModules.push(mod.name);
                             i++;
@@ -167,16 +167,16 @@
             }
         };
 
-        this.$get = ['$document', '$injector', '$interval', 'cacheMod', function ($document, $injector, $interval, cacheMod) {
+        this.$get = ['$document', '$injector', '$interval', 'cacheMod', '$http', '$compile', function($document, $injector, $interval, cacheMod, $http, $compile) {
             return {
-                loadScript: function (module, afterLoad) {
+                loadScript: function(module, afterLoad) {
                     if (req) {
                         //Подгружаем модуль
-                        var loadProcess = $interval(function () {
+                        var loadProcess = $interval(function() {
                             if (!loadFlag) {
                                 myInjector = $injector;
                                 var inst = LoadInstance();
-                                inst.loadModule(module, function () {
+                                inst.loadModule(module, function() {
                                     $interval.cancel(loadProcess);
                                     loadFlag = false;
                                     afterLoad();
@@ -188,88 +188,57 @@
                         throw ("Для работы необходимо подключить RequireJS!");
                     }
                 },
-                getModuleConfig: function (moduleName) {
-                    var mod = modules[moduleName];
-                    if (mod) {
-                        return mod;
-                    } else {
-                        return null;
+                loadModule: function(options, afterLoad) {
+                    if (afterLoad === undefined) {
+                        afterLoad = function (){};
                     }
-                },
-                locRequire: function (url, afterLoad) {
-                    console.log("без req");
-                    var cache = cacheMod.get(url[0]);
-                    if (!cache) {
-                        var script = $document[0].createElement('script');
-                        script.src = url[0];
-                        if (script.readyState) {
-                            script.onreadystatechange = function () {
-                                if (script.readyState === 'complete' || script.readyState === 'loaded') {
-                                    script.onreadystatechange = null;
-                                    afterLoad();
-                                }
-                            };
-                        } else {
-                            script.onload = function () { afterLoad(); };
-                        }
-                        script.onerror = function () {
-                            throw ('Не могу загрузить скрипт ' + url[0]);
-                        }
-                        $document[0].documentElement.appendChild(script);
-                        cacheMod.put(url[0], url[0]);
-                    } else {
-                        afterLoad();
+                    //Смотрим что грузить
+                    if (!options.name) {
+                        throw ("Не передано имя модуля!");
                     }
-                }
-            };
-        } ];
-        //Настройка модулей, которые необходимо подключить
-        this.config = function (config, mainApp) {
-            var mainModule = angular.module(mainApp);
-            //Модули которые уже подгружены в системе
-            angular.forEach(mainModule.requires, function (loadMod) {
-                regModules.push(loadMod);
-            });
-            if (angular.isArray(config)) {
-                angular.forEach(config, function (modConfig) {
-                    modules[modConfig.name] = modConfig;
-                });
-            } else {
-                modules[modConfig.name] = modConfig;
-            }
-        };
-    } ]);
+                    //Проверяем передан ли родительский scope
+                    if (!options.scope) {
+                        throw ("Не передан scope к которомы привязывать модуль!");
+                    } else {
+                        var scope = options.scope;
+                    }
+                    //Элемент в который нужно вставить подгруженный шаблон
+                    if (!options.el) {
+                        throw ("Вы не указали елемент в который нужно подгрузить шаблон!");
+                    } else {
+                        var elm = options.el;
+                    }
+                    //Определяем чем грузить
+                    if (!req) {
+                        req = this.locRequire;
+                    }
 
-    lazyLoad.directive("lazyLoad", function ($lazyLoad, $http, $compile, cacheMod) {
-        return {
-            link: function (scope, elm, attr) {
-                //console.log("дир");
-                //Получаем имя модуля, который необходимо подгрузить
-                var moduleName = attr.lazyLoad;
-                if (!req) {
-                    req = $lazyLoad.locRequire;
-                }
-                if (moduleName) {
-                    //Подгружаем скрип с модулем
-                    var modConfig = $lazyLoad.getModuleConfig(moduleName);
+
+                    var modConfig = this.getModuleConfig(options.name);
+
+
                     if (modConfig) {
-                        $lazyLoad.loadScript(modConfig, function () {
+                        this.loadScript(modConfig, function() {
                             //проверяем указан ли шаблон для модуля
                             if (modConfig.template) {
                                 var temp = cacheMod.get(modConfig.template);
                                 if (temp) {
-                                    childScope = scope.$new();
                                     elm.html(temp);
-                                    var content = elm.contents();
-                                    $compile(content)(childScope);
+                                    var content = elm.contents(),
+                                        sc = (options.newScope === undefined) ? scope : scope.$new();
+
+                                    $compile(content)(sc);
+                                    afterLoad();
                                 } else {
-                                    $http.get(modConfig.template).success(function (data) {
+                                    $http.get(modConfig.template).success(function(data) {
                                         //Создаем отдельное пространство для модуля
-                                        childScope = scope.$new();
                                         elm.html(data);
-                                        var content = elm.contents();
-                                        $compile(content)(childScope);
+                                        var content = elm.contents(),
+                                            sc = (options.newScope === undefined) ? scope : scope.$new();
+
+                                        $compile(content)(sc);
                                         cacheMod.put(modConfig.template, data);
+                                        afterLoad();
                                     });
                                 }
                             }
@@ -277,7 +246,67 @@
                     } else {
                         throw ("Вы пытаетесь загрузить модуль, который не описан в конфигурации загрузчика!");
                     }
+                },
+                getModuleConfig: function(moduleName) {
+                    var mod = modules[moduleName];
+                    if (mod) {
+                        return mod;
+                    } else {
+                        return null;
+                    }
+                },
+                locRequire: function(url, afterLoad) {
+                    var cache = cacheMod.get(url[0]);
+                    if (!cache) {
+                        var script = $document[0].createElement('script');
+                        script.src = url[0];
+                        if (script.readyState) {
+                            script.onreadystatechange = function() {
+                                if (script.readyState === 'complete' || script.readyState === 'loaded') {
+                                    script.onreadystatechange = null;
+                                    afterLoad();
+                                }
+                            };
+                        } else {
+                            script.onload = function() {
+                                afterLoad();
+                            };
+                        }
+                        script.onerror = function() {
+                            throw ('Не могу загрузить скрипт ' + url[0]);
+                        };
+                        $document[0].documentElement.appendChild(script);
+                        cacheMod.put(url[0], url[0]);
+                    } else {
+                        afterLoad();
+                    }
                 }
+            };
+        }];
+        //Настройка модулей, которые необходимо подключить
+        this.config = function(config, mainApp) {
+            var mainModule = angular.module(mainApp);
+            //Модули которые уже подгружены в системе
+            angular.forEach(mainModule.requires, function(loadMod) {
+                regModules.push(loadMod);
+            });
+            if (angular.isArray(config)) {
+                angular.forEach(config, function(modConfig) {
+                    modules[modConfig.name] = modConfig;
+                });
+            } else {
+                modules[modConfig.name] = modConfig;
+            }
+        };
+    }]);
+
+    lazyLoad.directive("lazyLoad", function($lazyLoad, $http, $compile, cacheMod) {
+        return {
+            link: function(scope, elm, attr) {
+                //Получаем имя модуля, который необходимо подгрузить
+                var moduleName = attr.lazyLoad, newScope = attr.newScope;
+
+                $lazyLoad.loadModule({name: moduleName, scope: scope, el: elm});
             }
         };
     });
